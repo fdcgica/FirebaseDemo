27342ca79163b91de3f5f2f67f49f97d28a97db4
FDC.Yan <fdcgica@gmail.com>
2023-05-17 00:11:19 +0800

    Instagram Clone
    Changed to Instagram Clone

--- app/src/main/java/com/example/firebasedemo/LoginActivity.java
+++ app/src/main/java/com/example/firebasedemo/LoginActivity.java
@@ -1,65 +1,14 @@
 package com.example.firebasedemo;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
-
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
-    private EditText email,password;
-    private Button loginBtn;
-    private FirebaseAuth auth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
-        email = findViewById(R.id.emailEt);
-        password = findViewById(R.id.passwordEt);
-        loginBtn = findViewById(R.id.loginButton);
-        auth = FirebaseAuth.getInstance();
-
-        loginBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                String txtEmail = email.getText().toString();
-                String txtPassword = password.getText().toString();
-                loginUser(txtEmail,txtPassword);
-            }
-        });
-
-    }
-
-    private void loginUser(String email, String password) {
-        auth.signInWithEmailAndPassword(email,password)
-                .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-                if (task.isSuccessful()) {
-                    // Login success
-                    FirebaseUser user = auth.getCurrentUser();
-                    Toast.makeText(LoginActivity.this, "Login successful", Toast.LENGTH_SHORT).show();
-                    startActivity(new Intent(LoginActivity.this,MainActivity.class));
-
-                    // Proceed to the next activity or perform other operations
-                } else {
-                    // Login failed
-                    Toast.makeText(LoginActivity.this, "Login failed", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
     }
 }
--- app/src/main/java/com/example/firebasedemo/RegisterActivity.java
+++ app/src/main/java/com/example/firebasedemo/RegisterActivity.java
@@ -9,76 +9,97 @@ import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class RegisterActivity extends AppCompatActivity {
-
-    private EditText email,password;
-    private Button submitBtn;
-    private FirebaseAuth auth;
+    private EditText username,name,email,password;
+    private Button register;
+    private TextView loginUser;
+    private DatabaseReference mRootRef;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        email = findViewById(R.id.emailEt);
-        password = findViewById(R.id.passwordEt);
-        submitBtn = findViewById(R.id.registerSubmit);
-        auth = FirebaseAuth.getInstance();
+        username = findViewById(R.id.username);
+        name = findViewById(R.id.name);
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        register = findViewById(R.id.register);
+        loginUser = findViewById(R.id.login_user);
 
-        submitBtn.setOnClickListener(new View.OnClickListener() {
+        mRootRef = FirebaseDatabase.getInstance().getReference();
+        mAuth = FirebaseAuth.getInstance();
+
+        loginUser.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+            }
+        });
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String txtUsername = username.getText().toString();
+                String txtName = name.getText().toString();
                 String txtEmail = email.getText().toString();
                 String txtPassword = password.getText().toString();
 
-                if(TextUtils.isEmpty(txtEmail) || TextUtils.isEmpty(txtPassword)){
-                    Toast.makeText(RegisterActivity.this, "Please input all Fields", Toast.LENGTH_SHORT).show();
+                if(TextUtils.isEmpty(txtUsername) || TextUtils.isEmpty(txtName) || TextUtils.isEmpty(txtEmail) || TextUtils.isEmpty(txtPassword)){
+                    Toast.makeText(RegisterActivity.this, "Please Input all fields", Toast.LENGTH_SHORT).show();
+                } else if (txtPassword.length() < 6) {
+                    Toast.makeText(RegisterActivity.this, "Password too short", Toast.LENGTH_SHORT).show();
                 }
-                else {
-                    registerUser(txtEmail,txtPassword);
+                else{
+                    registerUser(txtUsername,txtName,txtEmail,txtPassword);
                 }
             }
         });
     }
-    public void registerUser(String email, String password) {
-        auth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // User registration successful, log the user in
-                            loginUser(email, password);
-                            } else {
-                            // User registration failed, display error message
-                            Toast.makeText(getApplicationContext(), "Registration failed: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                });
-    }
+    private void registerUser(String username,String name, String email, String password){
+        mAuth.createUserWithEmailAndPassword(email,password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+            @Override
+            public void onSuccess(AuthResult authResult) {
 
-    public void loginUser(String email, String password) {
-        auth.signInWithEmailAndPassword(email, password)
-                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                HashMap<String, Object> map = new HashMap<>();
+                map.put("name", name);
+                map.put("email", email);
+                map.put("username", username);
+                map.put("id", mAuth.getCurrentUser().getUid());
+
+                mRootRef.child("Users").child(mAuth.getCurrentUser().getUid()).setValue(map).addOnCompleteListener(new OnCompleteListener<Void>() {
                     @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // User login successful, navigate to main activity
-                            Toast.makeText(RegisterActivity.this,"Registration Successful!", Toast.LENGTH_SHORT);
-                            Intent intent = new Intent(getApplicationContext(), StartActivity.class);
-                            startActivity(intent);
-                            finish();
-                        } else {
-                            // User login failed, display error message
-                            Toast.makeText(getApplicationContext(), "Login failed: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(RegisterActivity.this, "Update profile for better experience!", Toast.LENGTH_SHORT).show();
+                           Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+                           intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                           startActivity(intent);
+                           finish();
                         }
                     }
                 });
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(RegisterActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
