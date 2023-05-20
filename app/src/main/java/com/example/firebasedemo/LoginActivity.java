6f0effe2cd521220f93509970dced029b765ee8a
FDC.Yan <fdcgica@gmail.com>
2023-05-20 19:34:42 +0800

    Files Updated
    Added Login,Mainactivity pages

--- app/src/main/java/com/example/firebasedemo/LoginActivity.java
+++ app/src/main/java/com/example/firebasedemo/LoginActivity.java
@@ -1,14 +1,89 @@
 package com.example.firebasedemo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText mEmail, mPassword;
+    private TextView mUserRegister;
+    private Button mLogin;
+    FirebaseAuth mAuth;
+    ProgressDialog pd;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mEmail = findViewById(R.id.login_email);
+        mPassword = findViewById(R.id.login_password);
+        mLogin = findViewById(R.id.login_btn);
+        mUserRegister = findViewById(R.id.register_user);
+        pd = new ProgressDialog(this);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mLogin.setOnClickListener(this);
+        mUserRegister.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.login_btn:
+                String email = mEmail.getText().toString();
+                String password = mPassword.getText().toString();
+                if(TextUtils.isEmpty(email)||TextUtils.isEmpty(password)){
+                    Toast.makeText(LoginActivity.this, "Please input all required fields", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    loginUser(email,password);
+                }
+                break;
+            case R.id.register_user:
+                pd.setMessage("Please Wait");
+                pd.show();
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP));
+                finish();
+                break;
+            default:
+                return;
+        }
+    }
+    private void loginUser(String email, String password){
+        pd.setMessage("Please Wait");
+        pd.show();
+        mAuth.signInWithEmailAndPassword(email,password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+            @Override
+            public void onSuccess(AuthResult authResult) {
+                pd.dismiss();
+                startActivity(new Intent(LoginActivity.this, MainActivity.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP));
+                finish();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(LoginActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                pd.dismiss();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/firebasedemo/MainActivity.java
+++ app/src/main/java/com/example/firebasedemo/MainActivity.java
@@ -1,14 +1,59 @@
 package com.example.firebasedemo;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button mLogout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Binding
+        mLogout = findViewById(R.id.logout_btn);
+
+        mLogout.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        new AlertDialog.Builder(MainActivity.this)
+                .setTitle("About to exit the app")
+                .setMessage("Are you sure you want to exit?")
+                .setNegativeButton(android.R.string.no, null)
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+
+                    public void onClick(DialogInterface arg0, int arg1) {
+                        FirebaseAuth.getInstance().signOut();
+                        startActivity(new Intent(MainActivity.this, LoginActivity.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP));
+                        finish();
+                    }
+                }).create().show();
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.logout_btn:
+                FirebaseAuth.getInstance().signOut();
+                Toast.makeText(this,"You have signed out!", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(this, StartActivity.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP));
+                finish();
+                break;
+            default:
+                break;
+        }
     }
 }
