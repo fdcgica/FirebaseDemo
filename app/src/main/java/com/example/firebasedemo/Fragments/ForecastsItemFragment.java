628f82f390220603a8c6c425f6655056d0713470
FDC.Yan <fdcgica@gmail.com>
2023-05-29 17:55:25 +0800

    added this as well
    added this as well

---
+++ app/src/main/java/com/example/firebasedemo/Fragments/ForecastsItemFragment.java
@@ -0,0 +1,69 @@
+package com.example.firebasedemo.Fragments;
+
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.firebasedemo.R;
+public class ForecastsItemFragment extends DialogFragment {
+    public static ForecastsItemFragment newInstance(){
+        return new ForecastsItemFragment();
+    }
+
+    public ForecastsItemFragment() {
+        // Required empty public constructor
+    }
+
+    public static ForecastsItemFragment newInstance(String param1, String param2) {
+        ForecastsItemFragment fragment = new ForecastsItemFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+        }
+    }
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("Dialog Title")
+                .setMessage("Dialog Message")
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // Handle positive button click
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // Handle negative button click
+                    }
+                });
+
+        return builder.create();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_forecasts_item, container, false);
+    }
+}
